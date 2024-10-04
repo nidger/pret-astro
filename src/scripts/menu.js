79e81fae0bff5d506677ee647f60d4d52b8f9339
nidger <nprior70@yahoo.com>
2024-10-04 10:02:19 +0100

    added transition to mobile menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,6 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+document.querySelector('.hamburger').addEventListener('click', function() {
   document.querySelector('.nav-links').classList.toggle('expanded');
+  this.classList.toggle('active');
 });
 
 
